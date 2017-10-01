c36398e6a865605f8164685e25970796b3ef8062
dogosousa <dogosousa@gmail.com>
2017-10-01 05:59:33 -0300

    Fixing invelid geometries

--- intersection.py
+++ intersection.py
@@ -16,25 +16,41 @@ def intersection_shape_idx(filename1, filename2):
 
         with fiona.open(filename2, 'r') as layer2:
 
+            features = {}
             index = rtree.index.Index()
             for feat1 in layer1:
                 fid = int(feat1['id'])
                 geom1 = shape(feat1['geometry'])
                 index.insert(fid, geom1.bounds)
+                features[fid] = (feat1, geom1,)
 
             for feat2 in layer2:
                 geom2 = shape(feat2['geometry'])
                 for fid in list(index.intersection(geom2.bounds)):
-                    feat1 = layer1[fid]
-                    geom1 = shape(feat1['geometry'])
-                    if geom1.intersects(geom2):
-                        # print('{} intersects {}'.format(feat2['id'], feat1['id']))
-                        intersect = pd.DataFrame({'nome': [feat1['properties']['nome']],
-                                                  'Class_Name': [feat2['properties']['Class_Name']],
-                                                  'geometry': [geom1.intersection(geom2).to_wkt()],
-                                                   'int_area_m': [geom1.intersection(geom2).area],
-                                                  'area_munic': geom1.area},
-                                                 index = [feat2['id']])
-                        # result = result.append(intersect, ignore_index=True)
-                        result = pd.concat([result, intersect])
+                    feat1, geom1 = features[fid]
+                    try:
+                        if geom1.intersects(geom2):
+                            intersect = pd.DataFrame({'nome': [feat1['properties']['nome']],
+                                                      'Class_Name': [feat2['properties']['Class_Name']],
+                                                      'geometry': [geom1.intersection(geom2).to_wkt()],
+                                                       'int_area_m': [geom1.intersection(geom2).area],
+                                                      'area_munic': geom1.area},
+                                                     index = [feat2['id']])
+                            result = pd.concat([result, intersect])
+
+                    except Exception as e:
+                        print(e)
+                        if not geom1.is_valid:
+                            geom1 = geom1.buffer(0)
+                        if not geom2.is_valid:
+                            geom2 = geom2.buffer(0)
+                        if geom1.intersects(geom2):
+                            intersect = pd.DataFrame({'nome': [feat1['properties']['nome']],
+                                                      'Class_Name': [feat2['properties']['Class_Name']],
+                                                      'geometry': [geom1.intersection(geom2).to_wkt()],
+                                                       'int_area_m': [geom1.intersection(geom2).area],
+                                                      'area_munic': geom1.area},
+                                                     index = [feat2['id']])
+                            result = pd.concat([result, intersect])
+
         return result
